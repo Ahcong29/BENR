923198412d4645ef1468ab63097ad58296492f85
Ahcong29 <154807127+Ahcong29@users.noreply.github.com>
2023-12-28 00:16:57 +0800

    Update index.js

--- index.js
+++ index.js
@@ -65,23 +65,33 @@ async function run() {
     });
 
 /**
- * @swagger
+ *  @swagger
  * /regAdmin:
  *   post:
  *     summary: Register an Admin
+ *     description: Registers an Admin if not already registered
  *     requestBody:
  *       required: true
  *       content:
  *         application/json:
  *           schema:
- *             $ref: '#/components/schemas/Admin'
+ *             type: object
+ *             properties:
+ *               username:
+ *                 type: string
+ *               password:
+ *                 type: string
+ *               name:
+ *                 type: string
+ *               email:
+ *                 type: string
  *     responses:
  *       '200':
- *         description: Successful response
- *         content:
- *           application/json:
- *             schema:
- *               $ref: '#/components/schemas/ApiResponse'
+ *         description: Admin registered successfully or already exists
+ *       '500':
+ *         description: Internal server error
+ *     tags:
+ *       - Admin Registration
  */
     app.post('/regAdmin', async (req, res) => {
       let data = req.body;
